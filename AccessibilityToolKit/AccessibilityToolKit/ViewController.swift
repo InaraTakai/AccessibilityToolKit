6e25cc40eb7585be95dbf616f2aa3f5159c57fea
InaraTakai <49754894+InaraTakai@users.noreply.github.com>
2020-01-31 08:49:22 -0300

    Add files via upload

---
+++ AccessibilityToolKit/AccessibilityToolKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AccessibilityToolKit
+//
+//  Created by Inara Takashi on 31/01/20.
+//  Copyright © 2020 Inara Takashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
