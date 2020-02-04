25069e15b01dce66bd88c6bcaa8f9c1386cc255f
Inara <inaratakashi@gmail.com>
2020-02-04 15:45:08 -0300

    Estrutura estática das cartas

---
+++ AccessibilityToolKit/AccessibilityToolKit/Model/Card.swift
@@ -0,0 +1,21 @@
+//
+//  Card.swift
+//  AccessibilityToolKit
+//
+//  Created by Inara Takashi on 04/02/20.
+//  Copyright © 2020 Inara Takashi. All rights reserved.
+//
+
+import Foundation
+
+struct Card {
+    
+    var title: Title
+    var level: Level
+    var code: String?
+    var guideline: String?
+    var criterion: String?
+    var description: String?
+    var link: String?
+}
+
---
+++ AccessibilityToolKit/AccessibilityToolKit/Model/Title.swift
@@ -0,0 +1,16 @@
+//
+//  Title.swift
+//  AccessibilityToolKit
+//
+//  Created by Inara Takashi on 04/02/20.
+//  Copyright © 2020 Inara Takashi. All rights reserved.
+//
+
+import Foundation
+
+enum Title {
+    case Noticeable
+    case Operable
+    case Understandable
+    case Robust
+}
