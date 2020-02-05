e05c62e5da07d616ade910d8d93b10ddc7c21782
Gabriel Ferreira <ferreira.ribeirog@gmail.com>
2020-02-05 14:16:12 -0300

    tutorial, carta com cancel e sla mais o que

--- AccessibilityToolKit/AccessibilityToolKit/CardViewController.swift
+++ AccessibilityToolKit/AccessibilityToolKit/CardViewController.swift
@@ -9,14 +9,17 @@
 import UIKit
 
 class CardViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
     /*
     // MARK: - Navigation
 
