5db0b7b44cf7652fe6a0dcfbc8d9728b94bdfea3
Inara <inaratakashi@gmail.com>
2020-02-05 15:31:03 -0300

    Ultima versão

--- AccessibilityToolKit/AccessibilityToolKit/CardViewController.swift
+++ AccessibilityToolKit/AccessibilityToolKit/CardViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class CardViewController: UIViewController {
+        
+       @IBOutlet weak var stackTitleLevel: UIStackView!
+       @IBOutlet weak var guideline: UILabel!
+       @IBOutlet weak var stackTitleCode: UIStackView!
+       @IBOutlet weak var shortDescription: UILabel!
+       @IBOutlet weak var link: UIButton!
+       
+       var principleTitle = "operável"
+       var level = "baixo"
+       var criterionTitle = "conteúdo não textual"
+       var code = "1.1.1"
+       var diretriz = "Alternativas em texto"
+       var conteudo = "Todo conteúdo “não textual” deve trazer uma alternativa em texto para identificar o conteúdo."
+       
+       func setAccessibilityCard() {
+           self.stackTitleLevel.isAccessibilityElement = true
+           self.stackTitleCode.isAccessibilityElement = true
+           self.guideline.isAccessibilityElement = true
+           self.shortDescription.isAccessibilityElement = true
+           self.link.isAccessibilityElement = true
+           
+           self.stackTitleLevel.accessibilityLabel = "Carta do princípio " + principleTitle + "com nível de conformidade" + level
+           self.guideline.accessibilityLabel = "Recomendação da carta" + diretriz
+           self.stackTitleCode.accessibilityLabel = "Critério de sucesso número" + code + criterionTitle
+           self.shortDescription.accessibilityLabel = conteudo
+           self.link.accessibilityLabel = "Liink para consultar a diretriz completa"
+           
+       }
+       
+       
+       override func viewDidLoad() {
+           super.viewDidLoad()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+           self.setAccessibilityCard()
+           // Do any additional setup after loading the view.
+       }
     
     @IBAction func cancel(_ sender: Any) {
         dismiss(animated: true, completion: nil)
