4460b665175a4f0e1a33568942233142ee23f99b
Inara <inaratakashi@gmail.com>
2020-02-06 12:05:44 -0300

    Backend Card

--- AccessibilityToolKit/AccessibilityToolKit/Model/Level.swift
+++ AccessibilityToolKit/AccessibilityToolKit/Model/Level.swift
@@ -12,4 +12,18 @@ enum Level {
     case A
     case AA
     case AAA
+    
+    func level() -> String{
+        var levelLabel :  String
+        
+        switch self {
+        case .A:
+            levelLabel = NSLocalizedString("A", comment: "Nível de conformidade")
+        case .AA:
+            levelLabel = NSLocalizedString("AA", comment: "Nível de conformidade")
+        case .AAA:
+            levelLabel = NSLocalizedString("AAA", comment: "Nível de conformidade")
+        }
+        return levelLabel
+    }
 }
