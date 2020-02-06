0cfe08d80e04481a56c02570341b59477d9ed7f4
Gabriel Ferreira <ferreira.ribeirog@gmail.com>
2020-02-06 14:40:08 -0300

    tutorial, cell dynamic, card dynamic

---
+++ AccessibilityToolKit/AccessibilityToolKit/TutorialViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TutorialViewController.swift
+//  AccessibilityToolKit
+//
+//  Created by Gabriel Ferreira on 06/02/20.
+//  Copyright © 2020 Inara Takashi. All rights reserved.
+//
+
+import UIKit
+
+class TutorialViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
