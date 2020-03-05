632bff1d470d6f25e90098d7251bdba3782ad7ef
Inara <inaratakashi@gmail.com>
2020-03-05 15:53:56 -0300

    AllyKit - Version 1.0.1
    Version 1.0.1

--- AccessibilityToolKit/AccessibilityToolKit/AboutViewController.swift
+++ AccessibilityToolKit/AccessibilityToolKit/AboutViewController.swift
@@ -9,8 +9,9 @@
 import UIKit
 
 class AboutViewController: UIViewController {
-    @IBOutlet weak var image: UIImageView!
     
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var linkAbout: UIButton!
     @IBOutlet weak var aboutContent: UILabel!
     @IBOutlet weak var aboutLabel: UILabel!
     
@@ -20,8 +21,12 @@ class AboutViewController: UIViewController {
         self.image.tintColor = UIColor(named: "ToolKitBackOpposite")
         self.aboutLabel.text  = NSLocalizedString("Sobre o aplicativo", comment: "")
         self.aboutContent.text = NSLocalizedString("Esta versão do “toolkit” foi baseado nas cartas produzidas pelo designer UX, Marcelo Sales, com intuito de simplificar a compreensão das diretrizes WCAG (Web Content Accessibility Guidelines) e de torna-las acessíveis através desta plataforma digital. Utilize-a como fonte de pesquisa, consulta rápida, ferramenta para facilitação, disseminação de conhecimento e onde mais achar que o mesmo seja útil. A WCAG são diretrizes que fundamentam as recomendações básicas para a construção de conteúdos digitais de forma acessível a todos. Todo conteúdo adaptado neste material foi criado originalmente pelo W3C.", comment: "")
+        self.linkAbout.setTitle(NSLocalizedString("Link para licença de uso", comment: ""), for: .normal)
         
        
     }
 
+    @IBAction func linkBtn(_ sender: Any) {
+        UIApplication.shared.open(URL(string: "https://creativecommons.org/licenses/by-sa/4.0/deed.pt_BR")!)
+    }
 }
